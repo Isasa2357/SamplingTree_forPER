302d6c381924406c866acc93c12ed69066b63dd5
Isasa2357 <iwatake.s.b573@m.isct.ac.jp>
2025-07-10 17:30:44 +0900

    complete prototype

--- 1-make_SamplingTree/make_SamplingTree/make_SamplingTree/SamplingTree.h
+++ 1-make_SamplingTree/make_SamplingTree/make_SamplingTree/SamplingTree.h
@@ -1,10 +1,13 @@
 ﻿#pragma once
 
 #include <iostream>
+#include <ostream>
+#include <sstream>
 #include <vector>
 #include <random>
 #include <utility>
 #include <tuple>
+#include <format>
 
 #define TYPE double
 
@@ -18,22 +21,21 @@ private:
 
 	// get_sample関連
 	std::random_device _rd;
-	std::mt19937 _sampling_randEngine;
+	std::mt19937 _randEngine;
 
 	size_t _capacity;
 	size_t _real_size;
 
 	// max leaf
-	int max_leaf_idx;
+	int _max_leafIdx;
 
 public:
 	SamplingTree(const size_t capacity) 
-		: _write_idx(0), _capacity(capacity), _real_size(0)
+		: _write_idx(0), _capacity(capacity), _real_size(0), _max_leafIdx(capacity)
 	{
-
 		this->_tree.resize(2 * this->_capacity - 1);
 
-		this->_sampling_randEngine = std::mt19937(this->_rd());
+		this->_randEngine = std::mt19937(this->_rd());
 	}
 
 	/*
@@ -45,16 +47,20 @@ public:
 	*/
 	int add(const TYPE value) {
 		// 書き込み位置をtree_idxに変換
-		auto write_tree_idx = this->conv_leaf_idx2tree_idx(this->_write_idx);
+		auto write_treeIdx = this->conv_leafIdx2treeIdx(this->_write_idx);
 
 		// 更新差の記録
-		TYPE diff = value - this->_tree[write_tree_idx];
+		TYPE diff = value - this->_tree[write_treeIdx];
 
 		// 値を追加
-		this->_tree[write_tree_idx] = value;
+		this->_tree[write_treeIdx] = value;
+
+		// max_leafIdxの管理
+		if (value > this->_tree[this->_max_leafIdx]) this->_max_leafIdx = this->conv_treeIdx2leafIdx(write_treeIdx);
+		else if (write_treeIdx == this->_max_leafIdx) this->find_max_leaf();
 
 		// 木を修正
-		this->propagate_update(write_tree_idx, diff);
+		this->propagate_update(write_treeIdx, diff);
 
 		// write_idxとreal_sizeを更新
 		auto old_write_idx = this->_write_idx;
@@ -90,8 +96,27 @@ public:
 	Ret:
 		抽出したサンプルと抽出したサンプルのindex
 	*/
-	std::vector<TYPE> get_smaple(const size_t batch_size) {
-		//
+	std::pair<std::vector<TYPE>, std::vector<int>> get_smaples(const size_t sample_size) {
+		// keyの作成
+		std::vector<TYPE> keys(sample_size);
+		std::uniform_real_distribution<> key_dist(0.0, this->total());
+
+		for (auto& key : keys) key = key_dist(this->_randEngine);
+
+		// samplesの抽出
+		std::vector<TYPE> samples(sample_size);
+		std::vector<int> indics(sample_size);
+
+		for (int i = 0; i < keys.size(); ++i) {
+			auto ret = this->get_sample(keys[i]);
+			TYPE sample = ret.first;
+			int idx = ret.second;
+
+			samples[i] = sample;
+			indics[i] = idx;
+		}
+
+		return std::make_pair(samples, indics);
 	}
 
 	/*
@@ -101,7 +126,19 @@ public:
 		indics: 更新位置(leaf idx)
 	*/
 	void update(const std::vector<TYPE> values, const std::vector<int> indics) {
+		// 例外処理
+		if (values.size() != indics.size()) {
+			std::ostringstream except_oss;
+			except_oss << "SamplingTree::updat, values.size(" << values.size() << ") != indics.size(" << indics.size() << ")\n";
+			throw std::runtime_error(except_oss.str());
+		}
 
+		// 更新
+		for (auto i = 0; i < values.size(); ++i) {
+			TYPE diff = values[i] - this->_tree[indics[i]];
+			this->_tree[indics[i]] = values[i];
+			this->propagate_update(indics[i], diff);
+		}
 	}
 
 private:
@@ -122,6 +159,34 @@ private:
 		}
 	}
 
+	// ------------------------------ get_samples 関連 ------------------------------
+
+	/*
+	keyから値を取得する
+	*/
+	std::pair<TYPE, int> get_sample(double key) {
+		int retry_limit = 100;
+		for (int i = 0; i < retry_limit; ++i) {
+			int seek_idx = 0;
+			while (seek_idx < this->_capacity - 1) {
+				double lc = this->leftchild(seek_idx);
+				if (key <= lc) {
+					seek_idx = this->leftchild_idx(seek_idx);
+				} else {
+					key -= lc;
+					seek_idx = this->rightchild_idx(seek_idx);
+				}
+			}
+
+			if (this->conv_treeIdx2leafIdx(seek_idx) < this->_real_size)
+				return std::make_pair(this->_tree[seek_idx], this->conv_treeIdx2leafIdx(seek_idx));
+
+			std::cout << "miss\n";
+			key = std::uniform_real_distribution<double>(0, total())(this->_randEngine);
+		}
+		throw std::runtime_error("Failed to sample a valid element after multiple retries.");
+	}
+
 	// ------------------------------ state step ------------------------------
 
 	void step_write_idx() {
@@ -133,7 +198,16 @@ private:
 	}
 
 	// ------------------------------ max leaf 管理 ------------------------------
-
+	
+	/*
+	最大の葉を見つけ，その位置をmax_leafIdxに記録する
+	*/
+	void find_max_leaf() {
+		this->_max_leafIdx = this->get_first_leafpos_as_treeIdx();
+		for (int i = this->get_first_leafpos_as_treeIdx(); i <= this->get_last_leafpos_as_treeIdx(); ++i) {
+			if (this->_tree[this->_max_leafIdx] < this->_tree[i]) this->_max_leafIdx = i;
+		}
+	}
 
 
 private:
@@ -147,11 +221,17 @@ private:
 	inline TYPE rightchild(const int parent_idx) const { return this->_tree[this->rightchild_idx(parent_idx)]; }
 
 	// ------------------------------ leaf idx <-> tree idx ------------------------------
-	int conv_tree_idx2leaf_idx(const int treeidx) const { return treeidx - this->_capacity + 1; }
-	int conv_leaf_idx2tree_idx(const int leafidx) const { return this->_capacity - 1 + leafidx; }
-	bool is_leaf(const int tree_idx) { return (0 <= this->conv_tree_idx2leaf_idx(tree_idx) && this->conv_leaf_idx2tree_idx(tree_idx) < this->_capacity); }
+	int conv_treeIdx2leafIdx(const int treeIdx) const { return treeIdx - this->_capacity + 1; }
+	int conv_leafIdx2treeIdx(const int leafIdx) const { return this->_capacity - 1 + leafIdx; }
+	bool is_leaf(const int treeIdx) { return (0 <= this->conv_treeIdx2leafIdx(treeIdx) && this->conv_leafIdx2treeIdx(treeIdx) < this->_capacity); }
+
+	// ------------------------------ 意味のある変数を取得する ------------------------------
+	inline int get_first_leafpos_as_treeIdx() const { return this->_capacity; }						// 最初の葉の位置をtreeIdxで取得する
+	inline int get_last_leafpos_as_treeIdx() const { return this->_tree.size() - 1; }				// 最後の葉の位置をtreeIdxで取得する
+
 
 	// ------------------------------ getter ------------------------------
+public:
 	inline TYPE total() const { return this->_tree[0]; }
 	inline TYPE mal_leaf() const {}
 
